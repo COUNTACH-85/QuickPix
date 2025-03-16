172a81ab2228e7fe3d69be07be19c26c2d8195be
COUNTACH-85 <amiteshnayak15@gmail.com>
2025-03-16 21:47:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+const form = document.querySelector('.search');
+const input = document.querySelector(".keyword");
+const images = document.querySelectorAll("img");
+images.forEach((img)=> img.style.display = "none");
+
+const apiKey = "IGPVRNQ8CoJjGHSQk4uc91qfEAdDcgxNxFtb9juAIlk";
+
+form.addEventListener("submit", (event)=> {
+    event.preventDefault();     
+    let query = input.value.trim();
+    if (query === "") {
+        alert("Please enter a search term!");
+        return;
+    }    
+    images.forEach((img) => img.src = "");
+    fetch(`https://api.unsplash.com/search/photos?query=${query}&per_page=10&client_id=${apiKey}`)
+        .then((r) => r.json())
+        .then((data) => {
+            if (data.results.length === 0) {
+                alert("No images found!");
+                return;
+            }
+            const myarr = data.results.map((dataobj) => dataobj.urls.regular);
+            images.forEach((img, index) => {
+                
+                  img.src = myarr[index];
+                  img.style.display = "block";
+                
+            });
+        })
+        .catch((error) => alert("Error fetching images:", error));
+});
